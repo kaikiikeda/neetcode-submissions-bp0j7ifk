9d8fc7951f2caa5282e2cee39b1fb008b18583e9
kaikiikeda <123672973+kakimochi12@users.noreply.github.com>
2026-08-23 11:01:39 -1000

    Add: two-integer-sum - submission-92

---
+++ Data Structures & Algorithms/two-integer-sum/submission-92.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> prevmap;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int diff = target - nums[i];
+            if (prevmap.count(diff))
+            {
+                return {prevmap[diff], i};
+            }
+            prevmap[nums[i]] = i;
+        }
+    }
+};
