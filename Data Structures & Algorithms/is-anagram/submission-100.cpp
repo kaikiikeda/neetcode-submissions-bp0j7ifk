b278398fa70b730fd7ded64de0f2c4bc005ab605
kaikiikeda <123672973+kaikiikeda@users.noreply.github.com>
2026-09-07 10:57:16 -1000

    Add: is-anagram - submission-100

---
+++ Data Structures & Algorithms/is-anagram/submission-100.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> setS, setT;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            setS[s[i]]++;
+            setT[t[i]]++;
+        }
+        return setS == setT;
+    }
+};
